7ba7e6dd0c63f17beda0d21d1b08f9b1874117d3
Luan G S <luangabrielcap@hotmail.com>
2023-04-24 00:34:39 -0300

    java update

--- buildSrc/src/main/kotlin/gradle/android-library.gradle.kts
+++ buildSrc/src/main/kotlin/gradle/android-library.gradle.kts
@@ -1,10 +1,11 @@
 package gradle
 
-import extensions.addDefaultConfig
 import extensions.addCommonDependencies
-import extensions.addKoinDependencies
 import extensions.addCoroutinesDependencies
+import extensions.addDefaultConfig
+import extensions.addKoinDependencies
 import extensions.configureBuildTypes
+import gradle.kotlin.dsl.accessors._aaea1f5f099e0f580c925125501af1c8.java
 
 plugins {
     id("com.android.library")
@@ -21,9 +22,19 @@ android {
     configureBuildTypes()
 }
 
-dependencies{
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
     addCommonDependencies()
     addCoroutinesDependencies()
     addKoinDependencies()
 }
-
