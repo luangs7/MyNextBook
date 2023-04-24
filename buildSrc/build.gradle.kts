b9c1dff81e76aa5011f2d60a458d1284dd6b64af
Luan G S <luangabrielcap@hotmail.com>
2023-04-23 22:56:57 -0300

    quality gate with sonar

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `kotlin-dsl-precompiled-script-plugins`
+    id("org.sonarqube") version "3.5.0.2730"
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.1")
+    implementation("com.android.tools.build:gradle:7.4.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation("com.google.gms:google-services:4.3.15")
 }
@@ -18,6 +19,16 @@ dependencies {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xopt-in=requires-opt-in")
-        jvmTarget = "1.8"
+    }
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "luangs7_MyNextBook")
+        property("sonar.organization", "luangs7")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
