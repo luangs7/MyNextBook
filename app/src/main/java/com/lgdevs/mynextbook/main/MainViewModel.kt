df7674030eed71121f7355c149ec2fcac95686c6
Luan G S <luangabrielcap@hotmail.com>
2023-04-25 00:20:53 -0300

    unused imports

--- app/src/main/java/com/lgdevs/mynextbook/main/MainViewModel.kt
+++ app/src/main/java/com/lgdevs/mynextbook/main/MainViewModel.kt
@@ -6,7 +6,6 @@ import com.lgdevs.mynextbook.cloudservices.auth.CloudServicesAuth
 import com.lgdevs.mynextbook.cloudservices.messaging.CloudServicesMessaging
 import com.lgdevs.mynextbook.observer.KObserver
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
